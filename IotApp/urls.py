4e8a027063f69d559b9b35e481dc1f4d632f9cb9
Lavish987 <lavishchaddha284@gmail.com>
2023-10-31 20:02:00 +0530

    Reading Api created

--- IotApp/urls.py
+++ IotApp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path,include
 from rest_framework import routers
 from .views import DeviceViewSet
 
-router=routers.DefaultRouter()
-router.register('',DeviceViewSet)
+router=routers.DefaultRouter(trailing_slash=False)
+router.register(r'api/devices',DeviceViewSet)
 
 urlpatterns=[
-    path('api/devices/',include(router.urls)),  
+    path('',include(router.urls)),  
+
 ]
--- IotApp/views.py
+++ IotApp/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Device
-from .serializers import DeviceSerializer
+from rest_framework import viewsets,status
+from django.http import HttpResponse
+from .models import Device,TemperatureReading,HumidityReading
+from .serializers import DeviceSerializer,HumiditySerializer,TemperatureSerializer
+from rest_framework.decorators import action
 
 # Create your views here.
 class DeviceViewSet(viewsets.ModelViewSet):
     queryset=Device.objects.all()
     serializer_class=DeviceSerializer
+
+    @action(detail=True,methods=['get'])
+    def readings(self,request,device_id,parameter):
+        start_on = self.request.query_params.get('start_on')
+        end_on = self.request.query_params.get('end_on')
+        #print(start_on,end_on,device,parameter)
+        if parameter == 'temperature':
+                model = TemperatureReading
+                serializer_class = TemperatureSerializer
+        elif parameter == 'humidity':
+                model = HumidityReading
+                serializer_class = HumiditySerializer
+        else:
+            return HttpResponse({'error': 'Invalid parameter'}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = model.objects.filter(
+                device_id=device_id,
+                time__gte=start_on,
+                time__lte=end_on
+            )
+        return HttpResponse(data)
